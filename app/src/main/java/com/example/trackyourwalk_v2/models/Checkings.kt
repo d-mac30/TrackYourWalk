d8b2a63052d1a7615ea1747e17447506e7bd2ebe
d-mac30 <100619082+d-mac30@users.noreply.github.com>
2023-05-22 22:32:58 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/trackyourwalk_v2/models/Checkings.kt
@@ -0,0 +1,8 @@
+package com.example.trackyourwalk_v2.models
+
+data class Checkings(
+    var displayname: String,
+    var date: String,
+    var height: String,
+    var weight: String
+)
---
+++ app/src/main/java/com/example/trackyourwalk_v2/ui/settings/SettingsFragment.kt
@@ -0,0 +1,12 @@
+package com.example.trackyourwalk_v2.ui.settings
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+import com.example.trackyourwalk_v2.R
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+}
